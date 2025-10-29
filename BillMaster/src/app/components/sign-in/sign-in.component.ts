5916c20e4fb92f4d1d806bb871b818c2caefd6bd
vigneshwaran Rajamanickam <vigneshwaranr23@centizen.com>
2025-10-29 23:02:47 +0530

    signin component

---
+++ BillMaster/src/app/components/sign-in/sign-in.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in',
+  imports: [ReactiveFromModule],
+  templateUrl: './sign-in.component.html',
+  styleUrl: './sign-in.component.scss',
+  standalone:true
+})
+export class SignInComponent {
+
+}
